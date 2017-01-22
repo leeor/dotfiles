e011652b97a5f3f74a5f1298f559e34744eddb6d
Leeor Aharon <leeor.aharon@gmail.com>
2017-01-23 00:40:24 +0200

    switched to a symlink strategy

--- setup.sh
+++ setup.sh
@@ -1,11 +1,25 @@
 #!/usr/bin/env sh
 
 rcfiles=(
+	zsh/zshrc
+	zsh/zshrc.$(uname)
+	zsh/zshrc.plugins.$(uname)
+	zsh/shell_aliases
+	zsh/shell_config
+	zsh/shell_exports
+	zsh/shell_functions
+	"zsh/dircolors/dircolors.ansi-dark=>dircolors"
 	wget/wgetrc
+	"nvim/init.vim=>vimrc"
+	tmuxifier
 )
 
-for rcf in $rcfiles; do
-	if [ ! -h ~/.$(basename ${rcf}) ]; then
-		rm -f ~/.$(basename ${rcf}) && ln -s $(pwd)/${rcf} ~/.$(basename ${rcf})
-	fi
+for rcf in ${rcfiles[@]}; do
+	src=$(pwd)/${rcf}
+	dest=~/.$(basename ${rcf})
+	[[ "${rcf#*=>}" != "" ]] && src=$(pwd)/${rcf%=>*} && dest=~/.$(basename ${rcf#*=>})
+	rm -f ${dest} && ln -s ${src} ${dest}
+	unset dest
 done
+unset rcf
+unset rcfiles
--- zsh/README.md
+++ zsh/README.md
@@ -1,21 +1,8 @@
 # Set up XDG configuration structure
 
-In `/etc/zshenv`:
 ```
-if [[ -z "$XDG_CONFIG_HOME" ]]
-then
-  export XDG_CONFIG_HOME="$HOME/.config"
-fi
-
-if [[ -z "$XDG_CACHE_HOME" ]]
-then
-  export XDG_CACHE_HOME="$HOME/.cache"
-fi
-
-if [[ -d "$XDG_CONFIG_HOME/zsh" ]]
-then
-  export ZDOTDIR="$XDG_CONFIG_HOME/zsh"
-fi
+cd ~/.config
+./setup.sh
 ```
 
 # References
