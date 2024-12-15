94b7c93da43d84b585df0e8d561088e39c9a778a
Leeor Aharon <leeor.aharon@gmail.com>
2024-12-15 12:13:56 +0200

    codecompanion

--- nvim/lua/init.lua
+++ nvim/lua/init.lua
@@ -461,10 +461,79 @@ require("lazy").setup({
             return conf
         end
     },
+    -- AI
+    {
+        "olimorris/codecompanion.nvim",
+        dependencies = {
+            "nvim-lua/plenary.nvim",
+            "nvim-treesitter/nvim-treesitter",
+            "hrsh7th/nvim-cmp", -- Optional: For using slash commands and variables in the chat buffer
+            "nvim-telescope/telescope.nvim", -- Optional: For using slash commands
+            { "MeanderingProgrammer/render-markdown.nvim", ft = { "markdown", "codecompanion" } }, -- Optional: For prettier markdown rendering
+            { "stevearc/dressing.nvim", opts = {} }, -- Optional: Improves `vim.ui.select`
+        },
+        config = true
+    },
+    --{
+    --    "yetone/avante.nvim",
+    --    event = "VeryLazy",
+    --    lazy = false,
+    --    version = false, -- set this if you want to always pull the latest change
+    --    opts = {
+    --        -- add any opts here
+    --        behaviour = {
+    --            auto_suggestions = true
+    --        }
+    --    },
+    --    -- if you want to build from source then do `make BUILD_FROM_SOURCE=true`
+    --    build = "make",
+    --    -- build = "powershell -ExecutionPolicy Bypass -File Build.ps1 -BuildFromSource false" -- for windows
+    --    dependencies = {
+    --        "stevearc/dressing.nvim",
+    --        "nvim-lua/plenary.nvim",
+    --        "MunifTanjim/nui.nvim",
+    --        --- The below dependencies are optional,
+    --        "nvim-tree/nvim-web-devicons", -- or echasnovski/mini.icons
+    --        "zbirenbaum/copilot.lua", -- for providers='copilot'
+    --        {
+    --            -- support for image pasting
+    --            "HakonHarnes/img-clip.nvim",
+    --            event = "VeryLazy",
+    --            opts = {
+    --                -- recommended settings
+    --                default = {
+    --                    embed_image_as_base64 = false,
+    --                    prompt_for_file_name = false,
+    --                    drag_and_drop = {
+    --                        insert_mode = true,
+    --                    },
+    --                    -- required for Windows users
+    --                    use_absolute_path = true,
+    --                },
+    --            },
+    --        },
+    --        {
+    --            -- Make sure to set this up properly if you have lazy=true
+    --            'MeanderingProgrammer/render-markdown.nvim',
+    --            opts = {
+    --                file_types = { "markdown", "Avante" },
+    --            },
+    --            ft = { "markdown", "Avante" },
+    --        },
+    --    },
+    --}
 })
 
 vim.lsp.handlers["textDocument/hover"] = vim.lsp.with(vim.lsp.handlers.hover, { border = "rounded" })
 vim.lsp.handlers["textDocument/signatureHelp"] = vim.lsp.with(vim.lsp.handlers.signature_help, { border = "rounded" })
+
+require("codecompanion").setup({
+    strategies = {
+        chat = {
+            adapter = 'anthropic'
+        }
+    }
+})
 
 local capabilities = require('cmp_nvim_lsp').default_capabilities()
 local lspconfig = require('lspconfig')
@@ -643,15 +712,16 @@ end)
 --
 -- copilot
 --
-vim.keymap.set('i', '<C-j>', 'copilot#Accept("\\<CR>")', {
-    expr = true,
-    replace_keycodes = false
-})
-vim.g.copilot_no_tab_map = true
-vim.keymap.set('i', '<C-x>', '<Plug>(copilot-dismiss)')
-vim.keymap.set('i', '<C-]>', '<Plug>(copilot-next)')
-vim.keymap.set('i', '<C-[>', '<Plug>(copilot-previous)')
-vim.keymap.set('i', '<C-l>', '<Plug>(copilot-accept-word)')
+--vim.keymap.set('i', '<C-j>', 'copilot#Accept("\\<CR>")', {
+--    expr = true,
+--    replace_keycodes = false
+--})
+--vim.g.copilot_no_tab_map = true
+--vim.keymap.set('i', '<C-x>', '<Plug>(copilot-dismiss)')
+--vim.keymap.set('i', '<C-]>', '<Plug>(copilot-next)')
+--vim.keymap.set('i', '<C-[>', '<Plug>(copilot-previous)')
+--vim.keymap.set('i', '<C-l>', '<Plug>(copilot-accept-word)')
+vim.keymap.set('n', '<M-CR>', ':Copilot panel<CR>')
 
 --
 -- set up iron
