343d0423cc6469a889ac034184713a800f75fb63
Leeor Aharon <leeor.aharon@gmail.com>
2024-12-15 12:17:57 +0200

    remove avante

--- nvim/lua/init.lua
+++ nvim/lua/init.lua
@@ -395,12 +395,6 @@ require("lazy").setup({
     { "Bilal2453/luvit-meta", lazy = true },
 
     -- autocomplete
-    {
-        'madox2/vim-ai',
-        config = function()
-            vim.g.vim_ai_chat = { options = { model = "gpt-4-turbo-preview" } }
-        end
-    },
     'hrsh7th/cmp-nvim-lsp',
     'hrsh7th/cmp-buffer',
     'hrsh7th/cmp-path',
@@ -474,54 +468,12 @@ require("lazy").setup({
         },
         config = true
     },
-    --{
-    --    "yetone/avante.nvim",
-    --    event = "VeryLazy",
-    --    lazy = false,
-    --    version = false, -- set this if you want to always pull the latest change
-    --    opts = {
-    --        -- add any opts here
-    --        behaviour = {
-    --            auto_suggestions = true
-    --        }
-    --    },
-    --    -- if you want to build from source then do `make BUILD_FROM_SOURCE=true`
-    --    build = "make",
-    --    -- build = "powershell -ExecutionPolicy Bypass -File Build.ps1 -BuildFromSource false" -- for windows
-    --    dependencies = {
-    --        "stevearc/dressing.nvim",
-    --        "nvim-lua/plenary.nvim",
-    --        "MunifTanjim/nui.nvim",
-    --        --- The below dependencies are optional,
-    --        "nvim-tree/nvim-web-devicons", -- or echasnovski/mini.icons
-    --        "zbirenbaum/copilot.lua", -- for providers='copilot'
-    --        {
-    --            -- support for image pasting
-    --            "HakonHarnes/img-clip.nvim",
-    --            event = "VeryLazy",
-    --            opts = {
-    --                -- recommended settings
-    --                default = {
-    --                    embed_image_as_base64 = false,
-    --                    prompt_for_file_name = false,
-    --                    drag_and_drop = {
-    --                        insert_mode = true,
-    --                    },
-    --                    -- required for Windows users
-    --                    use_absolute_path = true,
-    --                },
-    --            },
-    --        },
-    --        {
-    --            -- Make sure to set this up properly if you have lazy=true
-    --            'MeanderingProgrammer/render-markdown.nvim',
-    --            opts = {
-    --                file_types = { "markdown", "Avante" },
-    --            },
-    --            ft = { "markdown", "Avante" },
-    --        },
-    --    },
-    --}
+    {
+        'madox2/vim-ai',
+        config = function()
+            vim.g.vim_ai_chat = { options = { model = "gpt-4-turbo-preview" } }
+        end
+    },
 })
 
 vim.lsp.handlers["textDocument/hover"] = vim.lsp.with(vim.lsp.handlers.hover, { border = "rounded" })
